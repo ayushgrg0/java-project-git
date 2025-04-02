4943fffc5409cc11c504b3a52a02498f234d496f
ayushgrg0 <gurungayush982@gmail.com>
2025-04-02 12:47:31 +0545

    added all files

---
+++ 1.py
@@ -0,0 +1,21 @@
+import numpy as num
+
+# # Create a NumPy array
+# arr = num.array([1, 2, 3, 4, 5])
+
+# # Perform basic operations
+# print("Array:", arr)
+# print("Sum:", num.sum(arr))
+# print("Mean:", num.mean(arr))
+# print("Max:", num.max(arr))
+# print("Min:", num.min(arr))
+
+# # Reshape array
+# reshaped_arr = arr.reshape(1, 5)
+# print("Reshaped Array:", reshaped_arr)
+
+n = num.array([1,2,3,4, "a"])
+print(n)
+
+a = [1,"Ayush", 2, "Saurav"]
+print(a)
